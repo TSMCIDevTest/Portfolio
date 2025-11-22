e65d9a89dbbbfcc7e0501b81c3f03104da7d5ec0
CrazyBrad77 <starcodebrad2023@outlook.com>
2025-11-22 14:13:28 -0800

    Added a window store using zuztand and immer

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -3,8 +3,10 @@ import { Tooltip } from 'react-tooltip';
 import React, { useRef } from 'react'
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
+import useWindowStore from '#store/window';
 
 const Dock = () => {
+    const { openWindow, closeWindow, windows } = useWindowStore();
     const dockRef = useRef(null);
     useGSAP(() => {
         const dock = dockRef.current;
@@ -34,7 +36,19 @@ const Dock = () => {
             dock.removeEventListener('mouseleave', resetIcons);
         }
     }, [])
-    const toggleApp = (app) => {}
+    const toggleApp = (app) => {
+        if(!app.canOpen) return;
+        const window = windows[app.id];
+        if (!window) {
+            console.error(`Window not found for app ${app.id}`);
+            return;
+        }
+        if(window.isOpen) {
+            closeWindow(app.id);
+        } else {
+            openWindow(app.id);
+        }
+    }
   return (
     <section id='dock'>
         <div ref={dockRef} className='dock-container'>
